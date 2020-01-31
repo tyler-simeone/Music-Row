e50c158ede98ea8ee718d7c993f02838191711f1
Tyler Simeone <tyler.simeone@gmail.com>
2020-01-31 14:23:47 -0600

    refactored to make code DRY

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,35 +2,7 @@ const jumpstopRecords = [];
 const chattenRecords = [];
 const polarRecords = [];
 
-const createFunkArtist = (name, genre, age) => {
-    return {
-        "Artist": name,
-        "Genre": genre,
-        "Age": age
-    }
-}
-const createRapArtist = (name, genre, age) => {
-    return {
-        "Artist": name,
-        "Genre": genre,
-        "Age": age
-    }
-}
-const createCountryArtist = (name, genre, age) => {
-    return {
-        "Artist": name,
-        "Genre": genre,
-        "Age": age
-    }
-}
-const createBluegrassArtist = (name, genre, age) => {
-    return {
-        "Artist": name,
-        "Genre": genre,
-        "Age": age
-    }
-}
-const createPopArtist = (name, genre, age) => {
+const createArtist = (name, genre, age) => {
     return {
         "Artist": name,
         "Genre": genre,
@@ -38,14 +10,14 @@ const createPopArtist = (name, genre, age) => {
     }
 }
 
-const bruceAtikins = createCountryArtist("Bruce Atikins", "Country", 23);
-const jensenBrown = createPopArtist("Jensen Brown", "Pop", 20);
-const dreFunkz = createFunkArtist("Dre Funkz", "Funk", 25);
-const dustaGrimes = createRapArtist("Dusta Grimes", "Rap", 21);
-const bartholomewDanielson = createBluegrassArtist("Bartholomew Danielson", "Bluegrass", 23);
-const avileeDallas = createCountryArtist("Avilee Dallas", "Country", 19);
-const austinKinkaid = createPopArtist("Austin Kinkaid", "Pop", 22);
-const loyonceBranis = createRapArtist("Loyoncé Branis", "Rap", 27);
+const bruceAtikins = createArtist("Bruce Atikins", "Country", 23);
+const jensenBrown = createArtist("Jensen Brown", "Pop", 20);
+const dreFunkz = createArtist("Dre Funkz", "Funk", 25);
+const dustaGrimes = createArtist("Dusta Grimes", "Rap", 21);
+const bartholomewDanielson = createArtist("Bartholomew Danielson", "Bluegrass", 23);
+const avileeDallas = createArtist("Avilee Dallas", "Country", 19);
+const austinKinkaid = createArtist("Austin Kinkaid", "Pop", 22);
+const loyonceBranis = createArtist("Loyoncé Branis", "Rap", 27);
 
 jumpstopRecords.push(dreFunkz, dustaGrimes, loyonceBranis);
 chattenRecords.push(bruceAtikins, bartholomewDanielson, avileeDallas);
